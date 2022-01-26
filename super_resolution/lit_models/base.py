2b73e8a099de0cbc3eaaf19706d9391696fdba02
“Darshil <darshilfcbayern@gmail.com>
2022-01-26 02:14:45 +0000

    Adding baselitmodel

--- super_resolution/lit_models/base.py
+++ super_resolution/lit_models/base.py
@@ -6,7 +6,9 @@ import torch.nn.functional as F
 
 OPTIMIZER = 'SGD'
 LR = 1e-4
+MOMENTUM = 0.9
 LOSS = 'mse_loss'
+EPOCHS = 1000
 ONE_CYCLE_TOTAL_STEPS = 100
 
 def psnr(preds,target):
@@ -16,7 +18,7 @@ def psnr(preds,target):
 
 class PSNR(Metric):
     '''
-    Creating a custom PSNR metric implmenting the Pytorch Lightning's metric interface
+    Custom PSNR metric implmenting the Pytorch Lightning's metric interface
     '''
     def __init__(self,dist_sync_on_step = False):
         super().__init__(dist_sync_on_step = dist_sync_on_step)
@@ -34,6 +36,50 @@ class PSNR(Metric):
     def compute(self):
         return self.correct.float()/self.total
 
+class BaseLitModule(pl.LightningModule):
+    '''
+    Generic Pytorch-Lightning class that must by initialized with a pytorch module
+    '''
+    def __init__(self,model,args : argspace.Namespace = None):
+        super().__init__()
+        self.model = model
+        self.args = vars(args) if args is not None else {}
 
+        optimizer = self.args.get("optimizer",OPTIMIZER)
+        self.optimizer_class = getattr(torch.optim,optimizer)
 
+        self.lr = self.args.get("lr",LR)
+        self.momentum = self.args.get("momentum",MOMENTUM)
 
+        loss = self.args.get("loss",LOSS)
+        self.loss_fn = getattr(torch.nn.functional,loss)
+
+        self.one_cycle_max_lr = self.args.get("one_cycle_max_lr", None)
+        self.one_cycle_total_steps = self.args.get("one_cycle_total_steps", ONE_CYCLE_TOTAL_STEPS)
+
+        self.train_psnr = PSNR()
+        self.val_psnr = PSNR()
+        self.test_acc = PSNR()
+
+    @staticmethod
+    def add_to_argparse(parser):
+        parser.add_argument("--optimizer",type = str,default = OPTIMIZER,help = "optimizer class from torch.optim")
+        parser.add_argument("--lr",type = float,default = LR)
+        parser.add_argument("--one_cycle_max_lr",type = float,default = None)
+        parser.add_argument("--one_cycle_total_steps",type = int,default = ONE_CYCLE_TOTAL_STEPS)
+        parser.add_argument("--loss",type = str,default = LOSS,help = "loss function from torch.nn.functional")
+        return parser
+
+    def configure_optimizers(self):
+        optimizer = self.optimizer_class(self.parameters(), lr=self.lr)
+        if self.one_cycle_max_lr is None:
+            return optimizer
+        scheduler = torch.optim.lr_scheduler.OneCycleLR(
+            optimizer = optimizer, max_lr = self.one_cycle_max_lr, total_steps = self.one_cycle_total_steps
+        )
+        return {'optimizer':optimizer,'lr_scheduler':scheduler,'monitor': 'val_loss'}
+
+    def forward(self,x):
+        return self.model(x)
+
+    def training_step()       
