4529dea3a3bf75443812c764930cfc20210ce1d3
“Darshil <darshilfcbayern@gmail.com>
2022-01-27 06:12:20 +0000

    T91

--- super_resolution/data/T91.py
+++ super_resolution/data/T91.py
@@ -5,3 +5,10 @@ from torch.utils.data import random_split
 from torchvision import transforms
 from super_resolution.base_data_module import BaseDataModule
 
+class T91(BaseDataModule):
+    '''
+    Creating a T91 DataModule inheriting from the BaseDataModule class
+    '''
+    def __init__(self,args : argparse.Namespace) -> None:
+        super().__init__(args)
+        
