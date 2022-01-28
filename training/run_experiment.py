ba61cfdc38014091bbbbcc2e9338e3c6ac331e2a
“Darshil <darshilfcbayern@gmail.com>
2022-01-28 10:25:30 +0000

    run experiment

--- training/run_experiment.py
+++ training/run_experiment.py
@@ -19,4 +19,43 @@ def _import_class(module_and_class_name : str)-> type:
     class_ = getattr(module,class_name)
     return class_
 
-def _setup_parser()
+def _setup_parser():
+    '''
+    Setting up argument parser with data, model, trainer and other arguments
+    '''
+    parser = argparse.ArgumentParser(add_help = False)
+
+    # Addting trainer arguments
+    trainer_parser = pl.Trainer.add_argparse_args(parser)
+    trainer_parser._action_groups[1].title = "Trainer Args"  # pylint: disable=protected-access
+    parser = argparse.ArgumentParser(add_help=False, parents=[trainer_parser])
+
+    # Basic arguments
+    parser.add_argument("--data_class", type=str, default="T91")
+    parser.add_argument("--model_class", type=str, default="srcnn")
+    parser.add_argument("--load_checkpoint", type=str, default=None)
+
+    # Get the data and model classes, so that we can add their specific arguments
+    temp_args, _ = parser.parse_known_args()
+    data_class = _import_class(f"text_recognizer.data.{temp_args.data_class}")
+    model_class = _import_class(f"text_recognizer.models.{temp_args.model_class}")
+
+    # Get data, model, and LitModel specific arguments
+    data_group = parser.add_argument_group("Data Args")
+    data_class.add_to_argparse(data_group)
+
+    model_group = parser.add_argument_group("Model Args")
+    model_class.add_to_argparse(model_group)
+
+    lit_model_group = parser.add_argument_group("LitModel Args")
+    lit_models.BaseLitModel.add_to_argparse(lit_model_group)
+
+    parser.add_argument("--help", "-h", action="help")
+    return parser
+
+def main():
+    '''Function used to run the experiment'''
+
+    parser = _setup_parser()
+    
+
